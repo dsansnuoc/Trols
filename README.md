60288e0310be1e8ba2e938a4c21600962ef5c0b0
DavidSansNaudo <93116281+DavidSansNaudo@users.noreply.github.com>
2022-04-13 21:24:17 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+"# Trols" 
+"# Trols" 
---
+++ src/js/index.js
@@ -0,0 +1,31 @@
+$(document).ready(main);
+
+var contador = 1;
+
+function main() {
+    $('.menu_bar').click(function () {
+        // $('nav').toggle(); 
+
+        if (contador == 1) {
+            $('nav').animate({
+                left: '0'
+            });
+            contador = 0;
+        } else {
+            contador = 1;
+            $('nav').animate({
+                left: '-100%'
+            });
+        }
+
+    });
+
+};
+
+
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
